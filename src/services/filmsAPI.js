f41d40218a7cab9f937102bd771b561f7136a0de
arkadiibasovych <arkadiybasovich@gmai.com>
2021-03-22 06:41:54 +0200

    add router

---
+++ src/services/filmsAPI.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+const API_KEY = `913469bb1c1eef486d339f6ac122cc37`;
+
+const fetchTrendingsFilms = () => {
+  return axios
+    .get(`trending/all/day?api_key=${API_KEY}`)
+    .then(({ data }) => data.results);
+};
+
+const fetchSearchFilms = searchQuery => {
+  return axios.get(
+    `/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=true&query=${searchQuery}`,
+  );
+};
+
+const fetchMovieDetails = movie_id => {
+  return axios.get(`movie/{movie_id}?api_key=${API_KEY}&language=en-US`);
+};
+
+export { fetchTrendingsFilms, fetchSearchFilms, fetchMovieDetails };
